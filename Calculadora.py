b05ffe6d2babdd176d925d8402f6acafff5d79ad
Margarida Nogueira <margarida.nogueira.05@gmail.com>
2026-01-20 11:28:03 +0000

    Criação da Calculadora

---
+++ Calculadora.py
@@ -0,0 +1,12 @@
+num_1 = float(input("Insira o primeiro valor: "))
+num_2 = float(input("Insira o segundo valor: "))
+
+ad = num_1 + num_2
+sub = num_1 - num_2
+mul = num_1 * num_2
+div = num_1 / num_2
+
+print("A soma é: ", ad)
+print("A subtração é: ", sub)
+print("A multiplicação é: ", mul)
+print("A divisão é: ", div)
